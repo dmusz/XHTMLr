f2a389085d8b9409a6a63097379ffe033584a36b
dmusz <5026306+dmusz@users.noreply.github.com>
2021-03-25 13:00:47 +0100

    .NET 5 version of XHTMLr

---
+++ XHTMLr_NET/Form.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XHTMLr
+{
+	public class Form
+	{
+		#region Fields
+
+		private XElement _form;
+
+		#endregion
+
+		#region Constructors
+
+		public Form() : this(new XElement("form"))
+		{
+		}
+
+		public Form(XElement form)
+		{
+			_form = form;
+		}
+
+		#endregion
+
+		#region Properties
+
+		public string Action
+		{
+			get
+			{
+				return (string)_form.Attribute("action") ?? string.Empty;
+			}
+			set
+			{
+				_form.SetAttributeValue("action", value);
+			}
+		}
+
+		public XElement Element { get { return _form; } }
+
+		public string EncType
+		{
+			get
+			{
+				return ((string)_form.Attribute("enctype")).NotEmpty("application/x-www-form-urlencoded");
+			}
+			set
+			{
+				_form.SetAttributeValue("enctype", value);
+			}
+		}
+
+		public IEnumerable<string> Keys
+		{
+			get
+			{
+				return _form.Descendants()
+					.Select(x => x.Attribute("name"))
+					.Where(x => x != null)
+					.Select(x => x.Value)
+					.Where(x => !string.IsNullOrEmpty(x))
+					.Distinct();
+			}
+		}
+
+		public string Method
+		{
+			get
+			{
+				return ((string)_form.Attribute("method")).NotEmpty("get");
+			}
+			set
+			{
+				_form.SetAttributeValue("method", value);
+			}
+		}
+
+		#endregion
+
+		#region Indexers
+
+		public string this[string name]
+		{
+			get
+			{
+				return _form.Field(name);
+			}
+			set
+			{
+				_form.Field(name, value);
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		public static explicit operator XElement(Form form)
+		{
+			return form._form;
+		}
+
+		public static Form[] GetForms(string html)
+		{
+			return XDocument.Parse(XHTML.ToXml(html)).GetForms();
+		}
+
+		public static Form[] GetForms(XDocument xdoc)
+		{
+			return xdoc.GetForms();
+		}
+
+		public void Remove(string name)
+		{
+			_form.GetElementsByName(name).Remove();
+		}
+
+		public FormData Serialize()
+		{
+			return _form.Serialize();
+		}
+
+		public Tuple<string, byte[]> SerializeData(System.Text.Encoding encoding = null)
+		{
+			return _form.SerializeData(encoding);
+		}
+
+		public override string ToString()
+		{
+			return ToString(null);
+		}
+
+		public string ToString(System.Text.Encoding encoding)
+		{
+			encoding = encoding ?? System.Text.Encoding.Default;
+			return encoding.GetString(_form.SerializeData(encoding).Item2);
+		}
+
+		#endregion
+	}
+}
---
+++ XHTMLr_NET/WordCleanup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace XHTMLr
+{
+	public class WordCleanup
+	{
+		#region Fields
+
+		private static Regex rxFixes = new Regex(@"
+            (<!--(\w|\W)+?-->)
+                | (<title>(\w|\W)+?</title>)
+                | (\s?class=\w+)
+                | (\s+style='[^']+')
+                | (<(meta|link|/?o:|/?style|/?div|/?st\d|/?head|/?html|body|/?body|/?span|!\[)[^>]*?>)
+                | ((<[^>]+>)+&nbsp;(</\w+>)+)
+                | (\s+v:\w+=""[^""]+"")
+                | ((\n\r){2,})
+                | (\s+[a-z]+\=[a-z]+)
+            ", RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+
+		#endregion
+
+		#region Methods
+
+		public static string CleanWordHtml(string html) => rxFixes.Replace(html, string.Empty).Trim();
+
+		public static string FixEntities(byte[] html)
+		{
+			var ret = new StringBuilder();
+			int k;
+			for (int i = 0; i < html.Length; i++)
+			{
+				k = Convert.ToInt32(html[i]);
+				if ((k >= 127 || k < 32) && k != 10 && k != 9 && k != 13)
+				{
+					ret.AppendFormat("&#{0};", k);
+				}
+				else
+				{
+					ret.Append(Convert.ToChar(k));
+				}
+			}
+			return ret.ToString();
+		}
+
+		#endregion
+	}
+}
